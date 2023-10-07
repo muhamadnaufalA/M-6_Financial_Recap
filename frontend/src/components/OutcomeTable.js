de2a967e3ee58fb07c6c21f718d97451d7aaa010
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-10-07 11:11:22 +0700

    create outcome table

--- frontend/src/components/OutcomeTable.js
+++ frontend/src/components/OutcomeTable.js
@@ -1,39 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useHistory, Link} from 'react-router-dom';
 
 export default function OutcomeTable() {
+    const UserId = Cookies.get("userId");
+    const [outcomes, setOutcome] = useState([]);
+
+    useEffect(()=>{
+        getListOutcomeFunc(); 
+    }, []);
+
+    const getListOutcomeFunc = async () =>{
+        const response = await axios.get(`http://localhost:5000/users/${UserId}/Outcomes`);
+        setOutcome(response.data);
+    }
+
+    const deleteOutcome = async (id) => {
+        try{
+            await axios.delete(`http://localhost:5000/Outcomes/${id}`);
+            getListOutcomeFunc();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <div>ASDASFJ</div>
-        // <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-        //     {/* TABEL */}
-        //     <div className="columns mt-5 is-centered">
-        //         <div className="column is-half">
-        //             <table className="table is-striped is-fullwidth">
-        //                 <thead>
-        //                     <tr>
-        //                         <th>No</th>
-        //                         <th>Name</th>
-        //                         <th>Balance</th>
-        //                         <th>Tanggal Pemasukan</th>
-        //                         <th>Actions</th>
-        //                     </tr>
-        //                 </thead>
-        //                 <tbody>
-        //                     {incomes.map((income, index) => (
-        //                         <tr key={income.id}>
-        //                             <td>{index + 1}</td>
-        //                             <td>{income.name}</td>
-        //                             <td>{income.balance}</td>
-        //                             <td>{income.tanggal_pemasukan}</td>
-        //                             <td>
-        //                                 <Link to={`editIncome/${income.id}`} className="button is-small is-info">Edit</Link>
-        //                                 <button onClick={() => deleteIncome(income.id)} className="button is-small is-danger">Delete</button>
-        //                             </td>
-        //                         </tr>
-        //                     ))}
-        //                 </tbody>
-        //             </table>
-        //         </div>
-        //     </div>
-        // </section>
+        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+            {/* TABEL */}
+            <div className="columns mt-5 is-centered">
+                <div className="column is-half">
+                    <table className="table is-striped is-fullwidth">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Nominal</th>
+                                <th>Tanggal Pengeluaran</th>
+                                <th>Jenis Pengeluaran</th>
+                                <th>Category</th>
+                                <th>Wallet</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {outcomes.map((outcome, index) => (
+                                <tr key={outcome.id}>
+                                    <td>{index + 1}</td>
+                                    <td>{outcome.name}</td>
+                                    <td>{outcome.nominal}</td>
+                                    <td>{outcome.tanggal_pengeluaran}</td>
+                                    <td>{outcome.jenisPengeluaranId}</td>
+                                    <td>{outcome.categoryId}</td>
+                                    <td>{outcome.walletId}</td>
+                                    <td>
+                                        {/* <Link to={`editIncome/${outcome.id}`} className="button is-small is-info">Edit</Link> */}
+                                        <button onClick={() => deleteOutcome(outcome.id)} className="button is-small is-danger">Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
     )
 }
