8e0fe2ffc0886e2d12b759717fb79d9fe73e4a0d
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-10-20 17:05:30 +0700

    create report controller and route

--- backend/controllers/ReportController.js
+++ backend/controllers/ReportController.js
@@ -1,7 +1,70 @@
 import BudgetRule from "../models/BudgetRuleModel.js";
-import Category from "../models/CategoryModel.js";
-import Income from "../models/IncomeModel.js";
 import Outcome from "../models/OutcomeModel.js";
-import Wallet from "../models/WalletModel.js";
-import { Op } from 'sequelize';
+import { Sequelize } from 'sequelize';
 
+export const getOutcomeReport = async (req, res) => {
+    const today = new Date();
+    const start = new Date(today.getFullYear(), today.getMonth(), 1);
+    const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1);
+    const end = new Date(nextMonth - 1);
+
+    try {
+        const outcomeResponse = await Outcome.findAll({
+            where: {
+                userId: req.params.id,
+                tanggal_pengeluaran: {
+                [Sequelize.Op.and]: [
+                    { [Sequelize.Op.gte]: start },
+                    { [Sequelize.Op.lte]: end }
+                ]
+                }
+            },
+            attributes: [
+                "budgetruleId",
+                [Sequelize.fn("SUM", Sequelize.col("nominal")), "totalPengeluaran"]
+            ],
+            group: ["budgetruleId"]
+        });
+
+        const outcomeResponse2 = await Outcome.findAll({
+            where: {
+                userId: req.params.id,
+                tanggal_pengeluaran: {
+                [Sequelize.Op.and]: [
+                    { [Sequelize.Op.gte]: start },
+                    { [Sequelize.Op.lte]: end }
+                ]
+                }
+            },
+            attributes: [
+                "budgetruleId"
+            ],
+            include: [
+                {
+                    model: BudgetRule, 
+                    attributes: ['name']
+                }
+            ]
+        });
+
+        const data = [...outcomeResponse, ...outcomeResponse2];
+
+        // Melacak nama untuk setiap budgetruleId
+        const budgetruleNames = {};
+
+        data.forEach(item => {
+            const budgetruleId = item.budgetruleId;
+            if(item.budgetrule && item.budgetrule.name) {
+                budgetruleNames[budgetruleId] = item.budgetrule.name;
+            }
+        });
+
+        outcomeResponse.forEach((item) => {
+            item.name = budgetruleNames[item.budgetruleId]
+        });
+
+        res.status(200).json(outcomeResponse);
+    } catch (error) {
+        console.error(error.message);
+    }
+};
--- backend/routes/ReportRoute.js
+++ backend/routes/ReportRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { getOutcomeReport } from "../controllers/ReportController.js";
 
 const router = express.Router();
 
-
+router.get('/users/:id/report', getOutcomeReport);
 
 export default router;
