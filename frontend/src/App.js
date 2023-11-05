8f2ddecbbaca94bac563499b972bc88b4cee7286
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-11-05 07:33:29 +0700

    update routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Switch, Route} from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -17,179 +17,34 @@ import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-function App() { 
+function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <Login/>
-        </Route>
-
-        <Route exact path="/register">
-          <Register/>
-        </Route>
-
-         {/* Dashboard */}
-          <Route exact path="/dashboard">
-          <div className="wrapper">
-            <Sidebar/>
-              <div className="main">
-                <Navbar/>
-                <main className="content">
-                  <div className="container-fluid p-0">
-                    <Dashboard/>
-                  </div>
-                </main>
-                <Footer/>
-              </div>
-            </div>
-          </Route>
-
-        {/* Income Page */}
-        <Route exact path="/income">
-          <div className="wrapper">
-            <Sidebar/>
-            <div className="main">
-              <Navbar/>
-              <main className="content">
-                  <div className="container-fluid p-0">
-                    <Income/>
-                  </div>
-              </main>
-              <Footer/>
-            </div>
-          </div>
-        </Route>
-
-        {/* Edit Income Page */}
-        <Route exact path="/editIncome/:id">
-          <div className="wrapper">
-            <Sidebar/>
-              <div className="main">
-                <Navbar/>
-                <main className="content">
-                  <div className="container-fluid p-0">
-                    <EditIncome/>
-                  </div>
-                </main>
-                <Footer/>
-              </div>
-          </div>
-        </Route>
-
-        {/* Outcome Page */}
-        <Route exact path="/outcome">
-          <div className="wrapper">
-            <Sidebar/>
-            <div className="main">
-              <Navbar/>
-              <main className="content">
-                  <div className="container-fluid p-0">
-                    <Outcome/>
-                  </div>
-              </main>
-              <Footer/>
-            </div>
-          </div>
-        </Route>
-
-        {/* Edit Outcome Page */}
-        <Route exact path="/edit-outcome/:id">
-          <div className="wrapper">
-            <Sidebar/>
-              <div className="main">
-                <Navbar/>
-                <main className="content">
-                  <div className="container-fluid p-0">
-                    <EditOutcome/>
-                  </div>
-                </main>
-                <Footer/>
-              </div>
-          </div>
-        </Route>
-
-        {/* Budget Rule Page */}
-        <Route exact path="/budgetrule">
-          <div className="wrapper">
-            <Sidebar/>
-            <div className="main">
-              <Navbar/>
-              <main className="content">
-                  <div className="container-fluid p-0">
-                    <BudgetRule/>
-                  </div>
-              </main>
-              <Footer/>
-            </div>
-          </div>
-        </Route>
-
-        {/* Category Page */}
-        <Route exact path="/category">
-          <div className="wrapper">
-            <Sidebar/>
-            <div className="main">
-              <Navbar/>
-              <main className="content">
-                  <div className="container-fluid p-0">
-                    <Category/>
-                  </div>
-              </main>
-              <Footer/>
-            </div>
-          </div>
-        </Route>
-
-        {/* Edit Category Page */}
-        <Route exact path="/editCategory/:id">
-          <div className="wrapper">
-            <Sidebar/>
-              <div className="main">
-                <Navbar/>
-                <main className="content">
-                  <div className="container-fluid p-0">
-                    <EditCategory/>
-                  </div>
-                </main>
-                <Footer/>
-              </div>
-          </div>
-        </Route>
-
-        {/* Wallet Page */}
-        <Route exact path="/wallet">
-          <div className="wrapper">
-            <Sidebar/>
-              <div className="main">
-                <Navbar/>
-                <main className="content">
-                  <div className="container-fluid p-0">
-                  <Wallet/>
-                  </div>
-                </main>
-                <Footer/>
-              </div>
-          </div>
-        </Route>
-
-        {/* Edit Wallet Page */}
-        <Route exact path="/editWallet/:id">
-          <div className="wrapper">
-            <Sidebar/>
-              <div className="main">
-                <Navbar/>
-                <main className="content">
-                  <div className="container-fluid p-0">
-                  <EditWallet/>
-                  </div>
-                </main>
-                <Footer/>
-              </div>
-          </div>
-        </Route>
-
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
       </Switch>
+      <div className="wrapper">
+        <Sidebar />
+        <div className="main">
+          <Navbar />
+          <main className="content">
+            <Switch>
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/income" component={Income} />
+              <Route exact path="/editIncome/:id" component={EditIncome} />
+              <Route exact path="/outcome" component={Outcome} />
+              <Route exact path="/edit-outcome/:id" component={EditOutcome} />
+              <Route exact path="/budgetrule" component={BudgetRule} />
+              <Route exact path="/category" component={Category} />
+              <Route exact path="/editCategory/:id" component={EditCategory} />
+              <Route exact path="/wallet" component={Wallet} />
+              <Route exact path="/editWallet/:id" component={EditWallet} />
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
