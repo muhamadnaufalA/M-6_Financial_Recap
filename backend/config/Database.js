9f74ea84a4ed0a02b5517c4ad1e93a9e41508e47
Rekabriyan <rekabriyanch08@gmail.com>
2023-11-07 13:55:15 +0700

    pop up create, edit, delete

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('financial_report', 'postgres', 'assydiq123', {
+const db = new Sequelize('financial_report', 'postgres', '4321', {
     host: 'localhost',
     dialect: 'postgres', // Menggunakan PostgreSQLA
     port: 5432, // Port default PostgreSQL
