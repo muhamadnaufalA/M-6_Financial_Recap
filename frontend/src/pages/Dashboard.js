36a69b7edfaaf7860b63070ea41dee71ca173da7
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-11-10 12:37:56 +0700

    add balance information

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { BiSolidHelpCircle } from "react-icons/bi";
+import { BiWallet } from 'react-icons/bi';
+import { BiSolidShow } from 'react-icons/bi';
+import { BiSolidHide } from 'react-icons/bi';
 import moment from 'moment';
-// import Highcharts from 'highcharts';
-// import HighchartsReact from 'highcharts-react-official';
 
 function Dashboard() {
 
@@ -13,19 +14,21 @@ function Dashboard() {
 	const [budgetRules, setBudgetRules] = useState([]);
 	const [budgetRulesActual, setBudgetRulesActual] = useState([]);
 	const [budgetRuleList, setBudgetRuleList] = useState([]);
-  	const [categoryList, setCategoryList] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+	const [wallets, setWallets] = useState([]);
 
 	useEffect(()=>{
 		getRecap(); 
 		getBudgetRulesTarget();
 		getBudgetRulesActual();
 		getListBudgetRuleFunc();
-    getListCategoryFunc(); 
+    getListCategoryFunc();
+		getWalletsFunc(); 
 	}, []);
 
     const getRecap = async () => {
-        const response = await axios.get(`http://localhost:5000/users/${UserId}/recap`);
-        setRecap(response.data);
+			const response = await axios.get(`http://localhost:5000/users/${UserId}/recap`);
+			setRecap(response.data);
     }
 
 	const getBudgetRulesTarget = async () => {
@@ -48,68 +51,26 @@ function Dashboard() {
 		setCategoryList(response.data);
 	}
 
+	const getWalletsFunc = async () => {
+		const response = await axios.get(`http://localhost:5000/users/${UserId}/wallets`)
+		setWallets(response.data)
+	}
+
 	const formatRupiah = (angka) => {
 		const numberFormat = new Intl.NumberFormat("id-ID");
-		return `Rp. ${numberFormat.format(angka)}`;
+		return `Rp${numberFormat.format(angka)},00`;
   };
 	
 	const today = new Date();
 	const date = today.getDate();
 	const month = today.getMonth() + 1;
 	const year = today.getFullYear();
-  	let formattedDate;
-	if (date < 10){
-		 formattedDate = `${year}-${month}-0${date}`;
-	}else{
-		formattedDate = `${year}-${month}-${date}`;
-	}
-
-	// Filter and Pagination Daily Recap Start //
-	const [selectedCategoryDaily, setselectedCategoryDaily] = useState("All");
-	const [selectedBudgetRuleDaily, setselectedBudgetRuleDaily] = useState("All");
-	const [selectedDate, setSelectedDate] = useState(formattedDate);
-	const [selectedTransactionDaily, setselectedTransactionDaily] = useState("All");
-	const [currentPageDaily, setcurrentPageDaily] = useState(1);
-	const itemsPerPageDaily = 5;
-
-	let parts = selectedDate.split('-');
-
-	const indexOfLastItemDaily = currentPageDaily * itemsPerPageDaily;
-	const indexOfFirstItemDaily = indexOfLastItemDaily - itemsPerPageDaily;
-	const currentItemsDaily = Recap
-		.filter((recap) => {
-			const transactionMatchDaily = selectedTransactionDaily === "All" || recap.transaction_type === selectedTransactionDaily;
-			const categoryMatchDaily = selectedCategoryDaily === "All" || recap.category === selectedCategoryDaily;
-			const budgetRuleMatchDaily = selectedBudgetRuleDaily === "All" || recap.budgetrule === selectedBudgetRuleDaily;
-			const dateMatchDaily = parseInt(new Date(recap.tanggal).getDate(), 10) === parseInt(parts[2], 10);
-			const monthMatchDaily = parseInt(new Date(recap.tanggal).getMonth() + 1, 10) === parseInt(parts[1], 10);
-      		const yearMatchDaily = parseInt(new Date(recap.tanggal).getFullYear(), 10) === parseInt(parts[0], 10);
-			return transactionMatchDaily && categoryMatchDaily && budgetRuleMatchDaily && dateMatchDaily && monthMatchDaily && yearMatchDaily;
-		})
-		.slice(indexOfFirstItemDaily, indexOfLastItemDaily);
-
-	const filteredDailyRecap = Recap.filter((recap) => {
-		const transactionMatchDaily = selectedTransactionDaily === "All" || recap.transaction_type === selectedTransactionDaily;
-		const categoryMatchDaily = selectedCategoryDaily === "All" || recap.category === selectedCategoryDaily;
-		const budgetRuleMatchDaily = selectedBudgetRuleDaily === "All" || recap.budgetrule === selectedBudgetRuleDaily;
-		const dateMatchDaily = parseInt(new Date(recap.tanggal).getDate(), 10) === parseInt(parts[2], 10);
-		const monthMatchDaily = parseInt(new Date(recap.tanggal).getMonth() + 1, 10) === parseInt(parts[1], 10);
-		const yearMatchDaily = parseInt(new Date(recap.tanggal).getFullYear(), 10) === parseInt(parts[0], 10);
-		return transactionMatchDaily && categoryMatchDaily && budgetRuleMatchDaily && dateMatchDaily && monthMatchDaily && yearMatchDaily;
-	});
-	
-	const totalPagesDaily = Math.ceil(filteredDailyRecap.length / itemsPerPageDaily);
-	const handlePageChangeDaily = (newPage) => {
-		if (newPage >= 1 && newPage <= totalPagesDaily) {
-				setcurrentPageDaily(newPage);
-		}
-	};
 
 	// Filter and Pagination Monthly Recap Start //
 	const [selectedCategoryMonthly, setSelectedCategoryMonthly] = useState("All");
 	const [selectedBudgetRuleMonthly, setSelectedBudgetRuleMonthly] = useState("All");
 	const [selectedMonthMonthly, setSelectedMonthMonthly] = useState(month);
-  	const [selectedYearMonthly, setSelectedYearMonthly] = useState(year);
+  const [selectedYearMonthly, setSelectedYearMonthly] = useState(year);
 	const [selectedTransactionMonthly, setSelectedTransactionMonthly] = useState("All");
 	const [currentPageMonthly, setCurrentPageMonthly] = useState(1);
 	const itemsPerPageMonthly = 5;
@@ -122,7 +83,7 @@ function Dashboard() {
 			const categoryMatchMonthly = selectedCategoryMonthly === "All" || recap.category === selectedCategoryMonthly;
 			const budgetRuleMatchMonthly = selectedBudgetRuleMonthly === "All" || recap.budgetrule === selectedBudgetRuleMonthly;
 			const monthMatchMonthly = selectedMonthMonthly === "All" || parseInt(new Date(recap.tanggal).getMonth() + 1, 10) === parseInt(selectedMonthMonthly, 10);
-      		const yearMatchMonthly = selectedYearMonthly === "All" || parseInt(new Date(recap.tanggal).getFullYear(), 10) === parseInt(selectedYearMonthly, 10);
+      const yearMatchMonthly = selectedYearMonthly === "All" || parseInt(new Date(recap.tanggal).getFullYear(), 10) === parseInt(selectedYearMonthly, 10);
 			return transactionMatchMonthly && categoryMatchMonthly && budgetRuleMatchMonthly && monthMatchMonthly && yearMatchMonthly;
 		})
 		.slice(indexOfFirstItemMonthly, indexOfLastItemMonthly);
@@ -132,7 +93,7 @@ function Dashboard() {
 		const categoryMatchMonthly = selectedCategoryMonthly === "All" || recap.category === selectedCategoryMonthly;
 		const budgetRuleMatchMonthly = selectedBudgetRuleMonthly === "All" || recap.budgetrule === selectedBudgetRuleMonthly;
 		const monthMatchMonthly = selectedMonthMonthly === "All" || parseInt(new Date(recap.tanggal).getMonth() + 1, 10) === parseInt(selectedMonthMonthly, 10);
-    	const yearMatchMonthly = selectedYearMonthly === "All" || parseInt(new Date(recap.tanggal).getFullYear(), 10) === parseInt(selectedYearMonthly, 10);
+    const yearMatchMonthly = selectedYearMonthly === "All" || parseInt(new Date(recap.tanggal).getFullYear(), 10) === parseInt(selectedYearMonthly, 10);
 		return transactionMatchMonthly && categoryMatchMonthly && budgetRuleMatchMonthly && monthMatchMonthly && yearMatchMonthly;
 	});
 	
@@ -142,85 +103,73 @@ function Dashboard() {
 				setCurrentPageMonthly(newPage);
 		}
 	};
-
-	function getDaysInMonth(year, month) {
-		const daysInMonth = new Date(year, month, 0).getDate();
-		const daysArray = Array.from({ length: daysInMonth }, (_, i) => (i + 1).toString());
-		return daysArray;
-	}
-	  
 	// Filter and Pagination Monthly Recap End //
 
-	// // Konfigurasi grafik
-	// const optionsTarget = {
-	// 	chart: {
-	// 	type: 'pie', // Jenis grafik, misalnya, bar, line, pie, dll.
-	// 	},
-	// 	title: {
-	// 	text: 'Budget Rule Target',
-	// 	},
-	// 	series: [
-	// 	{
-	// 		name: 'Data',
-	// 		data: [
-	// 		{ name: 'Needs', y: 60 }, // 60% untuk "needs"
-	// 		{ name: 'Wants', y: 30 }, // 30% untuk "wants"
-	// 		{ name: 'Savings', y: 10 }, // 10% untuk "savings"
-	// 		],
-	// 	},
-	// 	],
-	// };
+	// Card Saldo
+	const [selectedWallet, setSelectedWallet] = useState("All");
+	const [isShown, setIsShown] = useState(true);
+  const handleClick = () => {
+    setIsShown(!isShown);
+  };
 
-	// const optionsActual = {
-	// 	chart: {
-	// 	  type: 'pie',
-	// 	},
-	// 	title: {
-	// 	  text: 'Budget Rule Actual',
-	// 	},
-	// 	series: [
-	// 	  {
-	// 		name: 'Data',
-	// 		data: [
-	// 		  { name: 'Needs', y: 60 }, // 60% untuk "needs"
-	// 		  { name: 'Wants', y: 30 }, // 30% untuk "wants"
-	// 		  { name: 'Savings', y: 10 }, // 10% untuk "savings"
-	// 		],
-	// 	  },
-	// 	],
-	//   };
-	  
+	const filteredWallet = wallets
+		.filter((wallet) => {
+			const walletMatch = selectedWallet === "All" || wallet.name === selectedWallet;
+			return walletMatch;
+		})
+
+	let totalSaldo = 0;
+	filteredWallet.forEach((wallet) => {
+		totalSaldo += wallet.balance;
+	});
+
 	
-	// const ChartTarget = () => (
-	// 	<HighchartsReact
-	// 	highcharts={Highcharts}
-	// 	options={optionsTarget}
-	// 	/>
-	// );
-
-	// const ChartActual = () => (
-	// 	<HighchartsReact
-	// 	highcharts={Highcharts}
-	// 	options={optionsActual}
-	// 	/>
-	// );
-	
-// 	<div className="col-12 col-lg-12 col-xxl-12 d-flex">
-// 	<div className="card flex-fill">
-// 		<div className="card-header">
-// 			<ChartTarget /> {/* Gunakan komponen Chart di sini */}
-// 			<ChartActual /> {/* Gunakan komponen Chart di sini */}
-// 		</div>
-// 	</div>
-// </div>
-
   return (
     <>
 			<h1 className="h3 mb-3">
 				Dashboard
 			</h1>
-			{/* Outcome Target */}
 			<div className="row">
+				{/* Saldo Start */}
+				<div className="col-6 col-lg-6 col-xxl-6 d-flex">
+					<div className="card flex-fill">
+						<div className="card-header">
+							<h5 className="card-title mb-0">Saldo</h5>
+						</div>
+						<div>
+							<div className="col-4 p-3">
+								<select
+									className="form-control mr-2"
+									value={selectedWallet}
+									onChange={(e) => setSelectedWallet(e.target.value)}
+								>
+									<option value="All">All Wallets</option>
+									{wallets.map((wallet) => (
+										<option key={wallet.id} value={wallet.name}>
+												{wallet.name}
+										</option>
+									))}
+								</select>
+							</div>
+						</div>
+						<div className="d-flex align-items-center justify-content-center my-3">
+							<BiWallet className="mx-3" style={{ fontSize: '45px' }} />
+							{isShown ? (
+								<span className="fs-2 fw-bold font-monospace">{formatRupiah(totalSaldo)}</span>
+							) : (
+								<span className="fs-2 fw-bold font-monospace">Rp **********</span>
+							)}
+							
+							{isShown ? (
+								<BiSolidShow className="fs-1 mx-3" onClick={handleClick} />
+							) : (
+								<BiSolidHide className="fs-1 mx-3" onClick={handleClick} />
+							)}
+						</div>
+					</div>
+				</div>
+				{/* Saldo End */}
+				{/* Outcome Target */}
 				<div className="col-6 col-lg-6 col-xxl-6 d-flex">
 					<div className="card flex-fill">
 						<div className="card-header">
@@ -239,7 +188,7 @@ function Dashboard() {
 										<td className="d-none d-md-table-cell">{budgetRule.name}</td>
 										<td className="d-none d-md-table-cell">{budgetRule.percentage}%</td>
 									</tr>
-														))}
+								))}
 							</tbody>
 						</table>
 					</div>
@@ -271,126 +220,7 @@ function Dashboard() {
 						</table>
 					</div>
 				</div>
-				{/* Daily Recap */}
-				<div className="col-12 col-lg-12 col-xxl-12 d-flex">
-					<div className="card flex-fill">
-						<div className="card-header">
-							<h5 className="card-title mb-0">Daily Recap</h5>
-						</div>
-						<div className="box">
-							{/* Filter Start */}
-							<div className="d-flex mb-5" style={{ width: "100%" }}>
-								<div className="col-2 px-1">
-									<select
-										className="form-control mr-2"
-										value={selectedTransactionDaily}
-										onChange={(e) => setselectedTransactionDaily(e.target.value)}
-										disabled={currentPageDaily !== 1}
-										title={currentPageDaily !== 1 ? "Kembali ke page awal untuk memilih jenis transaksi" : ""}
-									>
-										<option value="All">All Transactions</option>
-										<option value="Income">Income</option>
-										<option value="Outcome">Outcome</option>
-									</select>
-								</div>
-								<div className="col-2 px-1">
-									<select
-										className="form-control mr-2"
-										value={selectedBudgetRuleDaily}
-										onChange={(e) => setselectedBudgetRuleDaily(e.target.value)}
-										disabled={currentPageDaily !== 1}
-										title={currentPageDaily !== 1 ? "Kembali ke page awal untuk memilih budget rule" : ""}
-									>
-										<option value="All">All Budget Rules</option>
-										{budgetRuleList.map((budgetRule) => (
-												<option key={budgetRule.id} value={budgetRule.name}>
-														{budgetRule.name}
-												</option>
-										))}
-									</select>
-								</div>
-								<div className="col-2 px-1">
-									<select
-										className="form-control"
-										value={selectedCategoryDaily}
-										onChange={(e) => setselectedCategoryDaily(e.target.value)}
-										disabled={currentPageDaily !== 1}
-										title={currentPageDaily !== 1 ? "Kembali ke page awal untuk memilih category" : ""}
-									>
-										<option value="All">All Categories</option>
-										{categoryList.map((category) => (
-												<option key={category.id} value={category.name}>
-														{category.name}
-												</option>
-										))}
-									</select>
-								</div>
-								<div className="col-2 px-1">
-									<input
-										type="date"
-										className="input"
-										value={selectedDate}
-										onChange={(e) => setSelectedDate(e.target.value)}
-										disabled={currentPageDaily !== 1}
-										title={currentPageDaily !== 1 ? "Kembali ke page awal untuk memilih jenis transaksi" : ""}
-									/>
-								</div>
-								<div className="col-1 px-1">
-									<BiSolidHelpCircle 
-										style={{  }}
-										title="Filter hanya aktif ketika berada di page 1"
-									/>
-								</div>
-							</div>
-							{/* Filter End */}
-
-							<table className="table table-hover my-0">
-								<thead>
-									<tr>
-										<th className="d-none d-md-table-cell">Tanggal</th>
-										<th>Keterangan</th>
-										<th>Budget Rule</th>
-										<th>Kategori</th>
-										<th>Nominal</th>
-										<th>Wallet</th>
-									</tr>
-								</thead>
-								<tbody>
-									{currentItemsDaily.map((l) => (
-										<tr key={l.id}>
-											<td className="d-none d-md-table-cell">{moment(l.tanggal).format('DD-MM-YYYY')}</td>
-											<td className="d-none d-xl-table-cell">{l.keterangan}</td>
-											<td className="d-none d-md-table-cell">{l.budgetrule}</td>
-											<td className="d-none d-md-table-cell">{l.category}</td>
-											<td className="d-none d-xl-table-cell"><span className={l.transaction_type !== "Outcome" ? "badge bg-success" : "badge bg-danger"}>{formatRupiah(l.balance ? l.balance : l.nominal)}</span></td>
-											<td className="d-none d-md-table-cell">{l.wallet ? l.wallet : 'Belum ditentukan'}</td>
-										</tr>
-									))}
-								</tbody>
-							</table>
-							{/* Pagination buttons */}
-							<div className="pagination mt-5">
-								<button
-									className="button"
-									onClick={() => handlePageChangeDaily(currentPageDaily - 1)}
-									disabled={currentPageDaily === 1}
-								>
-									Prev
-								</button>
-								<div>
-									Page { currentPageDaily } of { totalPagesDaily } Total Pages ({(itemsPerPageDaily * (currentPageDaily-1)) + 1} - {(itemsPerPageDaily * (currentPageDaily-1)) + 5 > filteredDailyRecap.length ? filteredDailyRecap.length : (itemsPerPageDaily * (currentPageDaily-1)) + 5} of {filteredDailyRecap.length})
-								</div>
-								<button
-									className="button"
-									onClick={() => handlePageChangeDaily(currentPageDaily + 1)}
-									disabled={currentPageDaily === totalPagesDaily}
-								>
-									Next
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
+				
 				{/* Monthly Recap Table Start */}
 				<div className="col-12 col-lg-12 col-xxl-12 d-flex">
 					<div className="card flex-fill">
